3b2a1b0938869e143bb550671cbc7947ee032f55
TLSdesigns <100785442+TLSdesigns@users.noreply.github.com>
2022-05-10 11:02:50 -0400

    work so far
    push

--- assignments/NightModeProject/js/index.js
+++ assignments/NightModeProject/js/index.js
@@ -6,6 +6,10 @@ let box1 = document.getElementById('box-1');
 let box2 = document.getElementById('box-2');
 let box3 = document.getElementById('box-3');
 
+let content1 = document.getElementById('content-1');
+let content2 = document.getElementById('content-2');
+let content3 = document.getElementById('content-3');
+
 let heading1 = document.getElementById('heading-1');
 let heading2 = document.getElementById('heading-2');
 let heading3 = document.getElementById('heading-3');
@@ -27,15 +31,23 @@ function nightModeHandler(){
   box3.classList.toggle('box-day');
   box3.classList.toggle('box-night');
 
-  heading1.classList.toggle('heading-day')
-  heading1.classList.toggle('heading-night')
+  content1.classList.toggle('content-day');
+  content1.classList.toggle('content-night');
 
-  heading2.classList.toggle('heading-day')
-  heading2.classList.toggle('heading-night')
+  content2.classList.toggle('content-day');
+  content2.classList.toggle('content-night');
 
-  heading3.classList.toggle('heading-day')
-  heading3.classList.toggle('heading-night')
+  content3.classList.toggle('content-day');
+  content3.classList.toggle('content-night');
 
+  heading1.classList.toggle('heading-day');
+  heading1.classList.toggle('heading-night');
+
+  heading2.classList.toggle('heading-day');
+  heading2.classList.toggle('heading-night');
+
+  heading3.classList.toggle('heading-day');
+  heading3.classList.toggle('heading-night');
 }
 
 //Night Mode Event Handler
